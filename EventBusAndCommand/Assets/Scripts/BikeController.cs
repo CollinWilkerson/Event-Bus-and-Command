823bac099eb990b26c18930ecc1c888a356d0568
CollinWilkerson <156472381+CollinWilkerson@users.noreply.github.com>
2025-09-09 12:09:44 -0700

    start Bikecontroller

--- EventBusAndCommand/Assets/Scripts/BikeController.cs
+++ EventBusAndCommand/Assets/Scripts/BikeController.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class BikeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnEnable()
     {
-        
+        RaceEventBus.Subscribe(RaceEventType.COUNTDOWN, StartTimer);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
